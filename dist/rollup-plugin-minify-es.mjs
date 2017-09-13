f321983eecda3cebb959a7b11968e44ca38383be
SupremeTechnopriest <randylebeau@gmail.com>
2017-09-12 20:36:29 -0400

    ✨ update sourcemap options

--- dist/rollup-plugin-minify-es.mjs
+++ dist/rollup-plugin-minify-es.mjs
@@ -4,43 +4,47 @@ import { writeFileSync } from 'fs';
 import { minify } from 'uglify-es';
 
 function rollup_plugin_minify (minifyMap) {
-  return {
-    name: 'minify',
-    transformBundle: function transformBundle(code, option) {
-      Object.keys(minifyMap).forEach(function (format) {
-        if (option.format == format) {
-          // init opt for minify
-          var opt = minifyMap[format];
-          opt = typeof opt == 'string' ? {dest: opt} : opt;
+    return {
+        name: 'minify',
+        transformBundle: function transformBundle(code, option) {
+            Object.keys(minifyMap).forEach(function (format) {
+                if (option.format == format) {
+                    // Minify option
+                    var opt = minifyMap[format];
+                    opt = typeof opt == 'string' ? {dest: opt} : opt;
 
-          var dest = opt.dest;
-          if (!dest) { return console.error('no dest, the minify plugin will abort for', format) }
-          var destObj = path.parse(dest);
+                    // Destination
+                    var dest = opt.dest;
+                    if (!dest) { return console.error('no dest, the minify plugin will abort for', format) }
+                    var destObj = path.parse(dest);
+                    delete opt.dest;
 
-          // prepare opt
-          delete opt.dest;
+                    // Source Maps
+                    var hasMaps = false;
+                    var filename = destObj.base + '.map';
+                    if(opt.sourceMaps) {
+                        hasMaps = true;
+                        delete opt.sourceMaps;
+                        var url = opt.sourceMapUrl || filename;
+                        delete opt.sourceMapUrl;
+                        opt.sourceMap = { filename: filename, url: url };
+                    }
 
-          if(opt.sourceMaps) {
-            delete opt.sourceMaps;
-            var filename$1 = destObj.base + '.map';
-            var url = opt.sourceMapUrl || filename$1;
-            delete opt.sourceMapUrl;
-            opt.sourceMap = { filename: filename$1, url: url };
-          }
+                    // Minify
+                    var result = minify(code, opt);
 
-          var result = minify(code, opt);
-          // ensure the target folder exists
-          mkdirp.sync(destObj.dir);
-          writeFileSync(dest, result.code, 'utf8');
+                    // Write files
+                    mkdirp.sync(destObj.dir);
+                    writeFileSync(dest, result.code, 'utf8');
 
-          if (opt.sourceMaps) {
-            writeFileSync(path.join(destObj.dir, filename), result.map, 'utf8');
-          }
+                    if (hasMaps) {
+                        writeFileSync(path.join(destObj.dir, filename), result.map, 'utf8');
+                    }
 
+                }
+            });
         }
-      });
     }
-  }
 }
 
 export default rollup_plugin_minify;
--- src/index.js
+++ src/index.js
@@ -4,41 +4,45 @@ import { writeFileSync } from 'fs'
 import { minify } from 'uglify-es'
 
 export default function rollup_plugin_minify (minifyMap) {
-  return {
-    name: 'minify',
-    transformBundle(code, option) {
-      Object.keys(minifyMap).forEach(format => {
-        if (option.format == format) {
-          // init opt for minify
-          let opt = minifyMap[format]
-          opt = typeof opt == 'string' ? {dest: opt} : opt
+    return {
+        name: 'minify',
+        transformBundle(code, option) {
+            Object.keys(minifyMap).forEach(format => {
+                if (option.format == format) {
+                    // Minify option
+                    let opt = minifyMap[format]
+                    opt = typeof opt == 'string' ? {dest: opt} : opt
 
-          let dest = opt.dest
-          if (!dest) return console.error('no dest, the minify plugin will abort for', format)
-          const destObj = path.parse(dest)
+                    // Destination
+                    let dest = opt.dest
+                    if (!dest) return console.error('no dest, the minify plugin will abort for', format)
+                    const destObj = path.parse(dest)
+                    delete opt.dest
 
-          // prepare opt
-          delete opt.dest
+                    // Source Maps
+                    let hasMaps = false
+                    const filename = destObj.base + '.map'
+                    if(opt.sourceMaps) {
+                        hasMaps = true
+                        delete opt.sourceMaps;
+                        const url = opt.sourceMapUrl || filename
+                        delete opt.sourceMapUrl;
+                        opt.sourceMap = { filename, url }
+                    }
 
-          if(opt.sourceMaps) {
-            delete opt.sourceMaps;
-            const filename = destObj.base + '.map'
-            const url = opt.sourceMapUrl || filename
-            delete opt.sourceMapUrl;
-            opt.sourceMap = { filename, url }
-          }
+                    // Minify
+                    const result = minify(code, opt)
 
-          const result = minify(code, opt)
-          // ensure the target folder exists
-          mkdirp.sync(destObj.dir)
-          writeFileSync(dest, result.code, 'utf8')
+                    // Write files
+                    mkdirp.sync(destObj.dir)
+                    writeFileSync(dest, result.code, 'utf8')
 
-          if (opt.sourceMaps) {
-            writeFileSync(path.join(destObj.dir, filename), result.map, 'utf8')
-          }
+                    if (hasMaps) {
+                        writeFileSync(path.join(destObj.dir, filename), result.map, 'utf8')
+                    }
 
+                }
+            })
         }
-      })
     }
-  }
 }
